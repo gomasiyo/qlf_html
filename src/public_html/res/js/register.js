774690fd2ca7bf7bc0b4793a8708cc8633a426b3
potato4d <mail@potato4d.me>
2015-11-01 11:06:09 +0900

    登録関連ひとまず実装。

---
+++ src/public_html/res/js/register.js
@@ -0,0 +1,76 @@
+$(function (){
+	var $form          = $("form#signup");
+	var $name          = $form.find("input[name='name']");
+	var $email         = $form.find("input[name='email']");
+	var $password      = $form.find("input[name='passwd']");
+	var $passwordAgain = $form.find("input[name='passwd-again']");
+
+	var passwordCheck = function (){
+		return $password.val() == $passwordAgain.val();
+	};
+
+	var duplicationCheck = function ($obj, type, sendData){
+		if(!$obj.val()){
+			$obj.parent().removeClass('has-error');
+			return false;
+		}
+
+		$.ajax({
+			url: DOMAIN+'/api/account/check/',
+			type: 'POST',
+			dataType: 'json',
+			data: sendData,
+		})
+		.done(function(data) {
+			if(data.status){
+				$obj.parent().addClass('has-error');
+			}else{
+				$obj.parent().removeClass('has-error');
+			}
+		})
+	};
+
+	$name.blur(function(event) {
+		duplicationCheck($name, "name", {name:$name.val()});
+	});
+
+	$email.blur(function(event) {
+		duplicationCheck($email, "email", {name:$email.val()});
+	});
+
+	$password.blur(function(event) {passwordCheck();});
+	$passwordAgain.blur(function(event) {passwordCheck();});
+
+	$form.submit(function(event) {
+		event.preventDefault();
+		console.log($form.serialize());
+
+		$.ajax({
+			url: DOMAIN+'/api/account/signup',
+			type: 'POST',
+			dataType: 'json',
+			data: $form.serialize(),
+		})
+		.done(function(data) {
+			console.log(data);
+
+			$.ajax({
+				url: DOMAIN+'/api/account/login',
+				type: 'POST',
+				dataType: 'json',
+				data: {
+					name   : $name.val(),
+					passwd : $password.val()
+				},
+			})
+			.done(function(data) {
+				console.log(data);
+				saveLoginData(data);
+				//location.href="/dashboard";
+			});
+
+		});
+		return false;
+	});
+	
+});
